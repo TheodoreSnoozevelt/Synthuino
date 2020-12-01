7b7aafb3fc7582355b576819a16469d189204e83
Theodore Snoozevelt <dummy@mail.com>
2020-12-01 22:46:51 +0100

    example song

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,11 @@
 #define AMP 127
 #define OFFSET 128
 #define PI2 6.283185
+#define SAMPLE_RATE 15625
 
 const float inv255 = 1 / 255.0;
 const float inv2048 = 1 / 2048.0;
-const float invSampleRate = 1 / 15625.0;
+const float invSampleRate = 1.0 / SAMPLE_RATE;
 
 bool doStep = false;
 
@@ -18,41 +19,34 @@ uint8_t triangleTable[LENGTH];
 
 OscValues synthOsc;
 OscValues synthOsc2;
-OscValues lfo;
-OscValues bassDrumOsc;
-OscValues envelopeGenerator;
 
-Oscillator bassOsc;
+ReleaseEnvelope<3> relOp;
+ReleaseEnvelope<3> relOp2;
 
-StepSequencer<float> melodySequencer;
+StepSequencer<float> melody1Sequencer;
+StepSequencer<float> melody2Sequencer;
 
-StepSequencer<int8_t> drumSequencer;
-int8_t currentDrumValue = 0;
 
-int8_t melody[32] = { 
-  0, -1, 0, 7, 5, -1, 3, -1, 
-  2, -1, 2, 2, 5, -1, 3, 2,
-  0, -1, 0, 15, 14, 15, 14, 15,
-  0, -1, 0, 15, 14, 15, 14, 15, 
+#define MELODY_SIZE 11
+int8_t melody[MELODY_SIZE] = { 
+  0, 5, 4, 1, 8, 7, 10, 5, 8, 4, 5
 };
-float melodyFreq[32];
-uint8_t melodyIndex = 0;
+float melodyFreq[MELODY_SIZE];
 
-int8_t drumBeat[2] = {1, 0};
-uint8_t drumIndex = 0;
+#define MELODY2_SIZE 5
+int8_t melody2[MELODY2_SIZE] = { 
+  0, 1, 0, 5, 4
+};
+float melody2Freq[MELODY2_SIZE];
 
-uint8_t envelope[256];
-uint8_t envelopeIndex = 0;
-uint8_t playEnvelope = 1;
-float currentEnvelopeValue = 0;
+#define BPM 240
+#define BPS 4
 
 int stepCounter = 0;
-byte dutyCycle = 0;
 
 #define BUFFERSIZE 800
 byte buffer[BUFFERSIZE];
-int bufferReadPos = 0;
-int bufferWritePos = BUFFERSIZE / 2;
+int bufferPos = 0;
 
 long stepMicros = 0;
 long totalMicros = 0;
@@ -63,32 +57,31 @@ long happySteps = 0;
 long sadSteps = 0;
 
 void initializeTables() {
-  for (int i=0; i < 32; i++) {
+  for (int i=0; i < MELODY_SIZE; i++) {
     int note = melody[i];
     if (note < 0) {
       melodyFreq[i] = 0;
     }
     else {
-      melodyFreq[i] = 440 * pow(2, note / 12.0);
+      melodyFreq[i] = 220 * pow(2, note / 12.0);
     }
   }
 
-  for (int i=0; i < 128; i++) {
-    float norm = i * 2 * inv255;
-    envelope[i] = 255 - pow(norm, 1.1) * 255;
+  for (int i=0; i < MELODY2_SIZE; i++) {
+    int note = melody2[i];
+    if (note < 0) {
+      melody2Freq[i] = 0;
+    }
+    else {
+      melody2Freq[i] = 110 * pow(2, note / 12.0);
+    }
   }
 
   for (int i=0; i<LENGTH; i++) { 
     float v = (AMP*sin((PI2/LENGTH)*i)); 
     sineTable[i] = int(v+OFFSET);
-    squareTable[i] = i < LENGTH/2 ? 255 : 0;
+    squareTable[i] = (i < LENGTH/2) ? 255 : 0;
     triangleTable[i] = i;
-    float norm = i * inv255;
-    envelope[i] = 255 - pow(norm, .3) * 255;
-    Serial.print("env(");
-    Serial.print(i);
-    Serial.print(") = ");
-    Serial.println(envelope[i]);
   }
 }
 
@@ -112,35 +105,24 @@ void initializeTimers() {
 }
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   initializeTables();
   initializeTimers();
 
-  melodySequencer.source = melodyFreq;
-  melodySequencer.steps = 32;
-  melodySequencer.duration = 4000;
+  synthOsc.length = 256;
+  synthOsc2.length = 256;
+  relOp.stepsPerReduction = 22;
+  relOp2.stepsPerReduction = 33;
 
-  drumSequencer.source = drumBeat;
-  drumSequencer.steps = 2;
-  drumSequencer.duration = 4000;
+  melody1Sequencer.duration = SAMPLE_RATE / 6;
+  melody1Sequencer.steps = MELODY_SIZE;
+  melody1Sequencer.source = melodyFreq;
+  melody1Sequencer.init();
 
-  synthOsc.length = LENGTH;
-  synthOsc.frequency = 220.0;
-  osc_calculate_phase_step(&synthOsc, invSampleRate);
-
-  synthOsc2.length = LENGTH;
-  synthOsc2.frequency = 110.0;
-  osc_calculate_phase_step(&synthOsc2, invSampleRate);
-
-  lfo.length = LENGTH;
-  lfo.frequency = 99.0;
-  osc_calculate_phase_step(&lfo, invSampleRate);
-
-  envelopeGenerator.length = LENGTH;
-  envelopeGenerator.phaseStep = 0;
-
-  bassDrumOsc.length = LENGTH;
-  bassDrumOsc.frequency = 220;
+  melody2Sequencer.duration = SAMPLE_RATE / 8;
+  melody2Sequencer.steps = MELODY2_SIZE;
+  melody2Sequencer.source = melody2Freq;
+  melody2Sequencer.init();
 }
 
 void loop() {
@@ -149,78 +131,60 @@ void loop() {
   if (doStep) {
     doStep = false;
 
-    stepCounter++;
-    
-    OSC_STEP(synthOsc, sineTable);
-    OSC_STEP(synthOsc2, sineTable);
-    OSC_STEP(lfo, sineTable);
-    OSC_STEP(bassDrumOsc, sineTable);
-    OSC_STEP(envelopeGenerator, envelope);
-
-    melodySequencer.step();
-    drumSequencer.step();
-
-
-    if (currentDrumValue == 0 && drumSequencer.output == 1) {
-      currentDrumValue = 1;
-      envelopeGenerator.frequency = 1.5;
-      OSC_CALCULATE_PHASE_STEP(envelopeGenerator, invSampleRate);
-    }
-
-    if (currentDrumValue == 1 && drumSequencer.output == 0) {
-      currentDrumValue = 0;
-      envelopeGenerator.phaseStep = 0;
-      envelopeGenerator.currentStep = 0;
-      bassDrumOsc.phaseStep = 0;
-      bassDrumOsc.output = 0;
-      bassDrumOsc.currentStep = 0;
-    }
-
-    if (envelopeGenerator.output < 100) {
-      envelopeGenerator.phaseStep = 0;
-      envelopeGenerator.currentStep = 0;
-      bassDrumOsc.phaseStep = 0;
-    }
-
-
-    if (stepCounter == 50) {
-      if (currentDrumValue == 1) {
-        bassDrumOsc.frequency = 500.0 * envelopeGenerator.output * inv255;
-        OSC_CALCULATE_PHASE_STEP(bassDrumOsc, invSampleRate);
-      }
-
-      synthOsc.frequency = melodySequencer.output;
+    if (stepCounter == 0) {
+      synthOsc.frequency = melody1Sequencer.output;
       OSC_CALCULATE_PHASE_STEP(synthOsc, invSampleRate);
-      synthOsc2.frequency = melodySequencer.output * 4.0;
-      OSC_CALCULATE_PHASE_STEP(synthOsc2, invSampleRate);
-      stepCounter = 0;
     }
-    
-    int val = 
-    //OCR2B = 
-      ((synthOsc.output)>>2) + 
-     /// (synthOsc2.output >> 3) + 
-      ((bassDrumOsc.output)>>2);
 
-    if (bufferWritePos == BUFFERSIZE)
-      bufferWritePos = 0;
-    buffer[bufferWritePos++] = val > 255 ? 255 : val;      
+    if (stepCounter == 1) {
+      synthOsc2.frequency = melody2Sequencer.output;
+      OSC_CALCULATE_PHASE_STEP(synthOsc2, invSampleRate);
+    }
+
+    stepCounter++;
+    if (stepCounter > 100)
+      stepCounter = 0;
+
+    OSC_STEP(synthOsc2, triangleTable);
+    OSC_STEP(synthOsc, squareTable);
+
+    relOp.inputGate = melody1Sequencer.gate;
+    relOp.inputValue = synthOsc.output;
+    relOp.step();
+
+    relOp2.inputGate = melody2Sequencer.gate;
+    relOp2.inputValue = synthOsc2.output;
+    relOp2.step();
+
+
+    melody1Sequencer.step();
+    melody2Sequencer.step();
+        
+    int val = (relOp2.outputValue >> 2) + (relOp.outputValue >> 2);
+    OCR2B = val;
+    if (doStep)
+      sadSteps++;
+    else
+      happySteps++;
   }
 
   if (totalIterations > 100000) {
     float timePerIteration = (float)totalMicros / totalIterations;
-    //Serial.print("Time per step: ");
-    //Serial.print(timePerIteration, 5);
-    //Serial.println(" microseconds");
-    //Serial.print("capacity used: ");
+    Serial.print(F("Time per step: "));
+    Serial.print(timePerIteration, 5);
+    Serial.println(F(" microseconds"));
+    Serial.print(F("capacity used: "));
     Serial.print(timePerIteration / maxTimePerIteration * 100);
     Serial.println("%");
+    Serial.print(F("Number of good/bad steps: "));
+    Serial.print(happySteps);
+    Serial.print(" ");
+    Serial.print(sadSteps);
+    Serial.print(" (");
+    Serial.print((float)sadSteps * 100 / happySteps);
+    Serial.println(")");
     totalIterations = 0;
     totalMicros = 0;
-
-    Serial.print(bufferReadPos);
-    Serial.print(" ");
-    Serial.println(bufferWritePos);
   }
   
   int diff = micros() - stepMicros;
@@ -230,7 +194,4 @@ void loop() {
 
 ISR(TIMER1_COMPA_vect) { 
   doStep = true;
-  if (bufferReadPos == BUFFERSIZE)
-    bufferReadPos = 0;
-  OCR2B = buffer[bufferReadPos++];
 }
