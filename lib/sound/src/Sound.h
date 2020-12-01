f15bab3e3e9991f4c99c6da6f3f21771aa208888
Theodore Snoozevelt <dummy@mail.com>
2020-12-01 22:46:29 +0100

    reduced accuracy for oscillator frequency, release envelope, gate output for step sequencer

--- lib/sound/src/Sound.h
+++ lib/sound/src/Sound.h
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 
-const unsigned long maxPhaseStep = 1L << 31;
+const unsigned long maxPhaseStep = 1L << 18;
 
 struct OscValues {
     unsigned short length;
@@ -14,13 +14,13 @@ inline void osc_step(OscValues *values, byte *array) {
     values->currentStep += values->phaseStep;
     if (values->currentStep >= maxPhaseStep)
         values->currentStep -= maxPhaseStep;
-    values->output = array[values->currentStep >> 24];
+    values->output = array[values->currentStep >> 10];
 }
 
 #define OSC_STEP(val, arr) val.currentStep += val.phaseStep; \
     if (val.currentStep >= maxPhaseStep) \
         val.currentStep -= maxPhaseStep; \
-    val.output = arr[val.currentStep >> 24];
+    val.output = arr[val.currentStep >> 10];
 
 #define OSC_CALCULATE_PHASE_STEP(val, inv) val.phaseStep = inv * val.frequency * maxPhaseStep;
 
@@ -52,6 +52,39 @@ class Oscillator {
         }
 };
 
+template<int T> class ReleaseEnvelope {
+    private:
+        int currentStep = 0;
+        byte amplitude = 0;
+    public:
+        byte inputGate;
+        byte inputValue;
+        int stepsPerReduction;
+        byte outputValue;
+
+        void step() {
+            if (inputGate > 0) {
+                amplitude = 255;
+                currentStep = 0;
+                return;
+            }
+
+            if (amplitude > 0) {
+                currentStep++;
+                if (currentStep == stepsPerReduction) {
+                    currentStep = 0;
+                    amplitude--;
+                }
+            }
+
+            int temp = inputValue << T;
+            for (int j = 255; j > amplitude; j -= (1 << (8 - T))) {
+                temp -= inputValue;
+            }
+            outputValue = temp >> T;
+        }
+};
+
 template<class T> class StepSequencer {
     private:
         int counter = 0;
@@ -61,12 +94,20 @@ template<class T> class StepSequencer {
         T output;
         int duration;
         int steps;
+        byte gate = 0;
+
+        void init() {
+            output = source[0];
+            gate = 1;
+        }
 
         void step() {
             counter++;
+            gate = 0;
             if (counter >= duration) {
                 counter = 0;
                 output = source[currentStep];
+                gate = 1;
                 currentStep++;
                 if (currentStep >= steps)
                     currentStep = 0;
